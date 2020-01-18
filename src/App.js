177db781c5f8c5022d64b999fcf8a92a73365e08
sofia <sofia.lanzinger@gmail.com>
2020-01-18 11:04:51 +0100

    react installed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            Get ready for a full-immersive tweet experience (with fireworks)
         </p>
         <a
           className="App-link"
