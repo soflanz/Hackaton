03b0d32513af3efcb4bbd35232e3b51249ad1711
sofia <sofia.lanzinger@gmail.com>
2020-01-18 11:32:06 +0100

    material ui installed + empezado tweet component

---
+++ src/components/tweet.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+/* styles */
+
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from '@material-ui/core/styles';
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles();
+
+function tweetsList() {
+    const classes = useStyles();
+
+    // tweetsData, length, error
+
+    // useEffect, fetch
+
+    return (
+
+    );
+}
+
+export default tweetsList;
